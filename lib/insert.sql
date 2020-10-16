0788723e3014cc245fe0f7ed6a9fc16c3a188df8
Bryce Moore <brycemooore@gmail.com>
2020-10-16 17:47:44 -0400

    Finisshed lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (1, 'Mr. Chocolate', 43, 'M', 'Brown', 'Chill', 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUEs (2, 'Rowdy', 23, 'M', 'White', 'Timid', 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUEs (3, 'Tabitha', 18, 'F', 'Black', 'Impatient', 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUEs (4, 'Sergeant Brown', 40, 'M', 'Brown', 'Direct', 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUEs (5, 'Melissa', 39, 'F', 'Grey', 'Eccentric', 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUEs (6, 'Grinch', 120, 'M', 'Green', 'Grumpy', 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (7, 'Wendy', 10, 'F', 'White', 'Sweet', 1);
+
+INSERT INTO bears (id, age, gender, color, temperament, alive)
+VALUES (8, 34, 'M', 'Black', 'Wicked', 1);
